3b354ac55d9078f8bdfb06727f84f8e7f98d481a
Basem Rizk <basem.rizk@outlook.com>
2018-11-17 15:53:29 +0100

    Adding (login - setting) menus to toolbar

--- app/src/main/java/com/example/and_lab/lab_5/LogIn.java
+++ app/src/main/java/com/example/and_lab/lab_5/LogIn.java
@@ -4,12 +4,17 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class LogIn extends AppCompatActivity {
 
     ActionBar actionBar;
 
     Toolbar m_toolbar;
+
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,36 @@ public class LogIn extends AppCompatActivity {
         app bar. For example, to hide the app bar, call ActionBar.hide().
          */
         actionBar = getSupportActionBar();
+    }
 
+    private void logIn(String username) {
+
+        actionBar.setTitle(username);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_login:
+                // User chose the "Login" item, show the app settings UI...
+                return true;
+
+            case R.id.action_settings:
+                // User chose the "Settings" action, mark the current item
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
