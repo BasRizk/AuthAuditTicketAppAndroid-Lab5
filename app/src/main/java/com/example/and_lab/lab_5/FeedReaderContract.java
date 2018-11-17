b6c454323fe420a9087d60a6fda1e3424b731dc3
Basem Rizk <basem.rizk@outlook.com>
2018-11-17 23:09:16 +0100

    DB Write and Read, and CSV File Creation

---
+++ app/src/main/java/com/example/and_lab/lab_5/FeedReaderContract.java
@@ -0,0 +1,63 @@
+package com.example.and_lab.lab_5;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+public class FeedReaderContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedEntry.COLUMN_NAME_USERNAME + " TEXT," +
+                    FeedEntry.COLUMN_NAME_TIMESTAMP + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+
+    private FeedReaderContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "LoginTimestamps";
+        public static final String COLUMN_NAME_USERNAME = "username";
+        public static final String COLUMN_NAME_TIMESTAMP = "timestamp";
+    }
+}
+
+
+class FeedReaderDbHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "FeedReader.db";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedReaderContract.FeedEntry.TABLE_NAME + " (" +
+                    FeedReaderContract.FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedReaderContract.FeedEntry.COLUMN_NAME_USERNAME + " TEXT," +
+                    FeedReaderContract.FeedEntry.COLUMN_NAME_TIMESTAMP + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedReaderContract.FeedEntry.TABLE_NAME;
+
+    public FeedReaderDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+
+}
--- app/src/main/java/com/example/and_lab/lab_5/LogIn.java
+++ app/src/main/java/com/example/and_lab/lab_5/LogIn.java
@@ -1,12 +1,27 @@
 package com.example.and_lab.lab_5;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.BaseColumns;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LogIn extends AppCompatActivity {
 
@@ -14,7 +29,9 @@ public class LogIn extends AppCompatActivity {
 
     Toolbar m_toolbar;
 
-    String username;
+    String current_username;
+    String current_timestamp;
+    FeedReaderDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +46,21 @@ public class LogIn extends AppCompatActivity {
         app bar. For example, to hide the app bar, call ActionBar.hide().
          */
         actionBar = getSupportActionBar();
+
+        /* Initializing the DB (Probably) */
+        mDbHelper = new FeedReaderDbHelper(this);
+
+
+        current_username = "Login-Timeline";
+        current_timestamp = "Login_Occurances";
+        saveToDB(current_username, current_timestamp);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        mDbHelper.close();
+        super.onDestroy();
     }
 
     private void logIn(String username) {
@@ -53,6 +85,11 @@ public class LogIn extends AppCompatActivity {
             case R.id.action_settings:
                 // User chose the "Settings" action, mark the current item
                 return true;
+            case R.id.action_exportCSV:
+                if(createCsvFile())
+                    Toast.makeText(getApplicationContext(),"CSV file created",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(),"Error creating CSV file",Toast.LENGTH_SHORT).show();
 
             default:
                 // If we got here, the user's action was not recognized.
@@ -61,4 +98,101 @@ public class LogIn extends AppCompatActivity {
 
         }
     }
+
+    private boolean createCsvFile() {
+        String filename = "timestamps.csv";
+
+        //Uri uri = Uri.parse("android.resource://" + getPackageName() + "/raw/" + filename);
+        Boolean write_successful = false;
+        File root;
+        try {
+            // <span id="IL_AD8" class="IL_AD">check for</span> SD card
+            root = Environment.getExternalStorageDirectory();
+            // TODO Just for now
+            Log.i("DB","path.." +root.getAbsolutePath());
+
+            //check sdcard permission
+            if (root.canWrite()){
+                File fileDir = new File(root.getAbsolutePath());
+                fileDir.mkdirs();
+
+                File file= new File(fileDir, filename);
+                FileWriter filewriter = new FileWriter(file);
+                BufferedWriter out = new BufferedWriter(filewriter);
+
+                out.write("Username,Timestamp");
+                ArrayList<String> tableEntries = readFromDB();
+                for(String row : tableEntries) {
+                    out.write(row);
+                }
+                out.close();
+
+                write_successful = true;
+            }
+        } catch (IOException e) {
+            Log.e("ERROR:---", "Could not write file to SDCard" + e.getMessage());
+            write_successful = false;
+        }
+        return write_successful;
+    }
+
+    private ArrayList<String> readFromDB() {
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        String[] projection = {
+                BaseColumns._ID,
+                FeedReaderContract.FeedEntry.COLUMN_NAME_USERNAME,
+                FeedReaderContract.FeedEntry.COLUMN_NAME_TIMESTAMP
+        };
+
+        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME_USERNAME + " = ?";
+        String[] selectionArgs = { "Username" };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                FeedReaderContract.FeedEntry.COLUMN_NAME_TIMESTAMP + " DESC";
+
+        Cursor cursor = db.query(
+                FeedReaderContract.FeedEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                null,              // The columns for the WHERE clause
+                null,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+
+        ArrayList<String> rows = new ArrayList();
+        while(cursor.moveToNext()) {
+            String username = cursor.getString(
+                    cursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_USERNAME));
+            String timestamp = cursor.getString(
+                    cursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_TIMESTAMP));
+            String row = username + "," + timestamp;
+            Log.d("DB", "Read entry from db = " + row);
+            rows.add(row);
+        }
+        cursor.close();
+
+        Log.d("DB", "DB finished reading data");
+
+        return rows;
+    }
+
+    private void saveToDB(String username, String timestamp) {
+        // Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_USERNAME, current_username);
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TIMESTAMP, current_timestamp);
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values);
+        Toast.makeText(getApplicationContext(),"New Entry in the DB with ID = " + newRowId,Toast.LENGTH_SHORT).show();
+
+    }
+
 }
